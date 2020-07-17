2efda8b8fddb210efd7ba079fae7a14233f4ba46
Josh Wyant <Joshwyant91@gmail.com>
2020-07-17 15:47:38 -0700

    FS WIP

--- kernel/fs.h
+++ kernel/fs.h
@@ -2,6 +2,10 @@
 #define __KERNEL_FS_H__
 
 #ifdef __cplusplus
+
+#include <sys/stat.h>
+#include <string.h>
+
 extern "C" {
 #endif
 
@@ -14,7 +18,13 @@ namespace kernel
 {
 class FileSystemNode
 {
-    
+public:
+    virtual void stat(struct stat *stat) = 0;
+    virtual KString name() = 0;
+};
+class FileSystemDriver
+{
+
 };
 }  // namespace kernel
 #endif  // __cplusplus
